496c822a338f400dd2f59f44eabeced5c8d5dd21
Francesco <119426925+xFr4nk@users.noreply.github.com>
2026-03-30 16:43:25 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'financehub-v1';
+const ASSETS = [
+  '/FinanceHub/',
+  '/FinanceHub/index.html',
+  '/FinanceHub/manifest.json'
+];
+
+// Installazione: metti in cache i file principali
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+// Attivazione: rimuovi cache vecchie
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch: prima prova la rete, poi la cache (così aggiornamenti funzionano)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
